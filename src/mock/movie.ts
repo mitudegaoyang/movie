7cf6630e16c44fda77e6fdc863c2ff1c08245462
Tianyang Gao <gaotianyang@iyiou.com>
2022-02-12 22:11:37 +0800

    feat: 新增json

--- src/mock/movie.ts
+++ src/mock/movie.ts
@@ -1,6 +1,8 @@
 import num1_16 from "./1-16-movie.json";
 import num17_35 from "./17-35-movie.json";
 import num36_43 from "./36-43-movie.json";
+import num44_56 from "./44-56-movie.json";
+import num57_59 from "./57-59-movie.json";
 import num150_154 from "./150-154-movie.json";
 import num170_189 from "./170-189-movie.json";
 import num190_217 from "./190-217-movie.json";
@@ -9,6 +11,8 @@ import format from "./format-movie.json";
 const MOVIELIST = num1_16
   .concat(num17_35)
   .concat(num36_43)
+  .concat(num44_56)
+  .concat(num57_59)
   .concat(num150_154)
   .concat(num170_189)
   .concat(num190_217)
