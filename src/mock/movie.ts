df62a3df7865c84369c9fcc0c62e537842be7d81
Tianyang Gao <gaotianyang@iyiou.com>
2022-02-13 17:16:37 +0800

    feat: 数据更新

--- src/mock/movie.ts
+++ src/mock/movie.ts
@@ -2,9 +2,12 @@ import num1_16 from "./1-16-movie.json";
 import num17_35 from "./17-35-movie.json";
 import num36_43 from "./36-43-movie.json";
 import num44_56 from "./44-56-movie.json";
-import num57_59 from "./57-59-movie.json";
+import num57_69 from "./57-69-movie.json";
+import num70_89 from "./70-89-movie.json";
+import num90_109 from "./90-109-movie.json";
+import num110_129 from "./110-129-movie.json";
 import num130_149 from "./130-149-movie.json";
-import num150_154 from "./150-154-movie.json";
+import num150_169 from "./150-169-movie.json";
 import num170_189 from "./170-189-movie.json";
 import num190_217 from "./190-217-movie.json";
 import num218_242 from "./218-242-movie.json";
@@ -13,9 +16,12 @@ const MOVIELIST = num1_16
   .concat(num17_35)
   .concat(num36_43)
   .concat(num44_56)
-  .concat(num57_59)
+  .concat(num57_69)
+  .concat(num70_89)
+  .concat(num90_109)
+  .concat(num110_129)
   .concat(num130_149)
-  .concat(num150_154)
+  .concat(num150_169)
   .concat(num170_189)
   .concat(num190_217)
   .concat(num218_242);
