36939d8979f39618a8b8e9e60fe1b891b3f19200
Tianyang Gao <gaotianyang@iyiou.com>
2022-02-13 11:45:31 +0800

    feat: 数据更新

--- src/mock/movie.ts
+++ src/mock/movie.ts
@@ -3,6 +3,7 @@ import num17_35 from "./17-35-movie.json";
 import num36_43 from "./36-43-movie.json";
 import num44_56 from "./44-56-movie.json";
 import num57_59 from "./57-59-movie.json";
+import num130_149 from "./130-149-movie.json";
 import num150_154 from "./150-154-movie.json";
 import num170_189 from "./170-189-movie.json";
 import num190_217 from "./190-217-movie.json";
@@ -13,6 +14,7 @@ const MOVIELIST = num1_16
   .concat(num36_43)
   .concat(num44_56)
   .concat(num57_59)
+  .concat(num130_149)
   .concat(num150_154)
   .concat(num170_189)
   .concat(num190_217)
