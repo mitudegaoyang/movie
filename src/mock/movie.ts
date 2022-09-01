67a145ad99b0866c3675a0ffaddfa65772167efb
迷途的羔羊 <13683265113@163.com>
2022-09-01 17:05:06 +0800

    feat: 优化请求数

--- src/mock/movie.ts
+++ src/mock/movie.ts
@@ -1,27 +1,11 @@
-// import new7k from "./new/7k.json";
-// import new6k from "./new/6k.json";
-// import new5k from "./new/5k.json";
-// import new4k from "./new/4k.json";
-// import new3k from "./new/3k.json";
-// import new2k from "./new/2k.json";
-// import new1k from "./new/1k.json";
-// import other7k from "./other/7k.json";
-// import other6k from "./other/6k.json";
-// import other5k from "./other/5k.json";
-// import other4k from "./other/4k.json";
-// import other3k from "./other/3k.json";
-// import other2k from "./other/2k.json";
-// import other1k from "./other/1k.json";
-// import top from "./top/1-517.json";
-// import format from "./format-movie.json";
 const MOVIELIST: any = [];
-const news = 63;
-for (let i = 0; i < news; i++) {
-  MOVIELIST.unshift(`new/${i + 1}`);
-}
-const other = 63;
+const other = 16;
 for (let i = 0; i < other; i++) {
   MOVIELIST.unshift(`other/${i + 1}`);
+}
+const news = 16;
+for (let i = 0; i < news; i++) {
+  MOVIELIST.unshift(`new/${i + 1}`);
 }
 export default MOVIELIST;
 
