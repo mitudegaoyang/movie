819b029274ad165746b820ec6319d0b5ce73a5d1
迷途的羔羊 <13683265113@163.com>
2022-11-01 08:59:26 +0800

    feat: 数据更新

---
+++ new.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+//系统路径模块
+let path = require("path");
+let formatList = require("./src/data/source/all-new.json");
+// 单个文件条目数
+let fileLength = 400;
+// 按文件条目拆分循环次数
+let num = Math.ceil(formatList.length / fileLength);
+console.log(`共${num}个最新类电影文件`);
+for (i = 0; i < num; i++) {
+  // 格式化json
+  let text = JSON.stringify(formatList.splice(-fileLength));
+
+  // 指定要创建的目录和文件名称 __dirname为执行当前js文件的目录
+  let file = path.join(__dirname + "/public/data/new", `${i + 1}.json`);
+
+  //写入文件
+  fs.writeFile(file, text, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (i === num - 1) {
+        console.log("文件创建成功~");
+      }
+    }
+  });
+}
